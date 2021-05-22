f3e7aaef631adfc63433bc2eacbb964db465e2c9
yahyaerdoan <77584301+yahyaerdoan@users.noreply.github.com>
2021-05-23 00:04:57 +0300

    Core.Utilities Katmanına Results Nesneleri Eklenlendi. Proje Results Türleri ile Yeniden Çalıştırıldı.

---
+++ northwind/src/main/java/kodlamaio/northwind/core/utilities/results/Result.java
@@ -0,0 +1,32 @@
+package kodlamaio.northwind.core.utilities.results;
+
+public class Result {
+
+	private boolean success;
+	private String message;
+	
+	public Result(boolean success) {
+		super();
+		this.success = success;
+	}
+	public Result(boolean success, String message) {
+		super();
+		this.success = success;
+		this.message = message;
+	}
+	
+	public boolean isSuccess() {
+		return success;
+	}
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+}
